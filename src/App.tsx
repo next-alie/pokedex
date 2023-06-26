4f9cfc17290ef80189ff3a1a1f38ef080a5c6176
Alie <bizcochito@anartist.org>
2023-06-26 17:50:32 +0200

    refactor with caching

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,15 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import PokeCard from "./PokeCard";
 import { PokemonClient } from "pokenode-ts";
-import { buildWebStorage } from "axios-cache-interceptor";
 
-const api = new PokemonClient({
-  cacheOptions: { storage: buildWebStorage(localStorage, "axios-cache:") },
-});
-
-// Initial fetch of pokemons
-async function fetchBatch(page: number): Promise<Object[]> {
-  let pokemons = [];
-  for (let i = page; i < page + 20; i++) {
-    const pokemon = await api.getPokemonById(i);
-    pokemons.push(pokemon);
-  }
-  return pokemons;
-}
+const api = new PokemonClient();
 
 function App() {
-  async function fetchPokemons(page) {
+  async function fetchPokemons() {
     try {
       // set loading to true before calling API
       setLoading(true);
-      const newPokemonList = await fetchBatch(page);
+      const newPokemonList = (await api.listPokemons(0, 9999)).results;
       setPokemonList(newPokemonList);
       // switch loading to false after fetch is complete
       setLoading(false);
@@ -34,13 +21,13 @@ function App() {
     }
   }
 
-  const [page, setPage] = useState(1);
-  const [pokemonList, setPokemonList] = useState({});
+  const [page, setPage] = useState(0);
+  const [pokemonList, setPokemonList] = useState([]);
   const [loading, setLoading] = useState(true);
 
   // useEffect with an empty dependency array works the same way as componentDidMount
   useEffect(() => {
-    fetchPokemons(page);
+    fetchPokemons();
   }, []);
 
   // return a Spinner when loading is true
@@ -55,20 +42,20 @@ function App() {
   function nextPage() {
     const newPage = page + 20;
     setPage(newPage);
-    fetchPokemons(newPage);
+    fetchPokemons();
   }
 
   function prevPage() {
     const newPage = page > 20 ? page - 20 : page;
     setPage(newPage);
-    fetchPokemons(newPage);
+    fetchPokemons();
   }
 
   return (
     <>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {pokemonList.map((pokemon) => {
-          return <PokeCard pokemon={pokemon} />;
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-6">
+        {pokemonList.slice(page, page + 20).map((pokemon) => {
+          return <PokeCard key={pokemon.name} name={pokemon.name} />;
         })}
       </div>
       <button
--- src/PokeCard.tsx
+++ src/PokeCard.tsx
@@ -1,4 +1,65 @@
-export default function PokeCard({ pokemon }) {
+import { useEffect, useState } from "react";
+import { PokemonClient } from "pokenode-ts";
+
+const api = new PokemonClient();
+
+export default function PokeCard({ name }) {
+  const [loading, setLoading] = useState(true);
+  const [pokemon, setPokemon] = useState({});
+
+  useEffect(() => {
+    async function loadPokemon() {
+      const newJson = localStorage.getItem("pokemon");
+      const newPokemon = newJson ? JSON.parse(newJson)[name] : null;
+      if (newPokemon) {
+        setPokemon(newPokemon);
+        setLoading(false);
+      } else {
+        try {
+          // set loading to true before calling API
+          setLoading(true);
+          const newPokemon = await api.getPokemonByName(name);
+          const usedPokemonValues = {
+            name: newPokemon.name,
+            sprites: newPokemon.sprites,
+            types: newPokemon.types,
+
+          }
+          try {
+            let pokemonJson = localStorage.getItem("pokemon");
+            let pokemons = {};
+            if (pokemonJson) {
+              pokemons = JSON.parse(pokemonJson);
+              pokemons[name] = usedPokemonValues;
+            } else {
+              pokemons[name] = usedPokemonValues;
+            }
+            localStorage.setItem("pokemon", JSON.stringify(pokemons));
+          } catch (error) {
+            console.log(error);
+          }
+          setPokemon(usedPokemonValues);
+          // switch loading to false after fetch is complete
+          setLoading(false);
+        } catch (error) {
+          // add error handling here
+          setLoading(false);
+          console.log(error);
+        }
+      }
+    }
+    loadPokemon();
+  }, []);
+
+  // return a Spinner when loading is true
+  if (loading)
+    return (
+      <img
+        src="icons/simple_pokeball.gif"
+        alt="Loading"
+      />
+    );
+
   return (
     <div className="w-full bg-white rounded-lg p-12 flex flex-col justify-center items-center">
       <div className="mb-8">
@@ -10,10 +71,10 @@ export default function PokeCard({ pokemon }) {
       </div>
       <div className="text-center">
         <p className="text-xl text-gray-700 font-bold mb-2">{pokemon.name}</p>
-        <p className="text-base text-gray-600 font-normal">
+        <div className="text-base text-gray-600 font-normal">
           {pokemon.types.map((type) => {
             return (
-              <p>
+              <div>
                 <img
                   className="inline"
                   src={
@@ -24,10 +85,10 @@ export default function PokeCard({ pokemon }) {
                   }
                   alt={type.type.name}
                 />
-              </p>
+              </div>
             );
           })}
-        </p>
+        </div>
       </div>
     </div>
   );
